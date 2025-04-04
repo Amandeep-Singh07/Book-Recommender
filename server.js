f8954cbdb37adfd6f4827a292fed9fdda67d603e
Amandeep Singh <amandeepronaldo@gmail.com>
2025-04-04 14:20:56 +0530

    Code Refactored

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", async (req, res) => {
-  res.sendFile("index.html", { root: "./" });
+  res.sendFile("/index.html", { root: "./" });
 });
 
 app.post("/get-response", async (req, res) => {
